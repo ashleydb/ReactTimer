9a7e1016604c1256e8ae8e93530fb8574f510fcd
ashleydb <abennett@gmail.com>
2016-12-08 17:03:00 -0800

    Making countdown work and testing it

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,54 @@ var CountdownForm = require('CountdownForm');
 var Countdown = React.createClass({
   getInitialState: function() {
     return {
-      count: 0
+      count: 0,
+      countdownStatus: 'STOPPED' //or 'PAUSED' or 'STARTED'
     };
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.countdownStatus != prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'STARTED':
+          this.startTimer();
+          break;
+        case 'STOPPED':
+          this.stopTimer();
+          break;
+        case 'PAUSED':
+          this.pauseTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function() {
+    //Every 1 second, edit the count
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      if (newCount >= 0) {
+        this.setState({
+          count: newCount
+        });
+      } else {
+        this.setState({
+          count: 0,
+          countdownStatus: 'STOPPED'
+        });
+      }
+    }, 1000);
+  },
+  stopTimer: function() {
+    this.timer = undefined;
+  },
+  pauseTimer: function() {
+    this.timer = undefined;
+    this.setState({
+      countdownStatus: 'PAUSED'
+    });
   },
   handleSetCountdown: function(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'STARTED'
     });
   },
   render: function() {
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should should set state to STARTED and count down', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      //Get the clock as a jQuery element
+      //var $el = $(ReactDOM.findDOMNode(countdown));
+
+      countdown.handleSetCountdown(5);
+      //var actualText = $el.find('.clock-text').text();
+      expect(countdown.state.countdownStatus).toBe('STARTED');
+      expect(countdown.state.count).toBe(5);
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(4);
+        done();
+      }, 1001);
+    });
+
+    it('should not count down below 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001);
+    });
+  });
+});
